57607bd8daa0f4971fb6a17feb02f9307a247c38
Ryan Schraeder <rschraeder2@gmail.com>
2024-03-14 15:48:02 -0600

    Update queries.py

--- queries.py
+++ queries.py
@@ -12,6 +12,24 @@ def teams():
         SELECT DISTINCT AWAY_TEAM_ID FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_STATS
     """
 
+def regular_season(start, end):
+    return f"""
+        SELECT DISTINCT 
+            DATE, 
+            AWAY_TEAM_ID, 
+            AWAY_GOALS,
+            HOME_TEAM_ID,
+            HOME_GOALS,
+            LENGTH_OF_GAME_MIN,
+            CASE WHEN 
+                HOME_GOALS = AWAY_GOALS AND HOME_GOALS = 0 AND AWAY_GOALS = 0
+                THEN 'UNKNOWN' ELSE OUTCOME END AS ACTUAL, 
+            PREDICTION
+        FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_STATS_PREDICTIONS 
+        WHERE DATE BETWEEN '{start}' and '{end}'
+        ORDER BY DATE DESC
+    """
+    
 def predictions_overview_query(start, end, away, home):
     return f"""
         SELECT DISTINCT 
