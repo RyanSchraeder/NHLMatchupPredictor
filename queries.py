059cf8fa18a6d9eab9ae106bf2848b46201dda4a
Ryan Schraeder <rschraeder2@gmail.com>
2025-04-07 09:34:52 -0600

    Update queries.py

--- queries.py
+++ queries.py
@@ -2,48 +2,38 @@ import streamlit as st
 
 def stats(start, end):
     return f"""
-        SELECT DISTINCT * FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_STATS 
-        WHERE DATE BETWEEN '{start}' and '{end}'
+        SELECT DISTINCT * FROM NHL_STATS.RAW.SEASONAL_METRICS_AGG
+        WHERE GAME_DATE BETWEEN '{start}' and '{end}'
         ORDER BY DATE DESC
     """
 
 def teams():
     return f"""
-        SELECT DISTINCT AWAY_TEAM_ID FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_STATS
+        SELECT DISTINCT TEAM FROM NHL_STATS.RAW.TEAMS
     """
 
 def regular_season(start, end):
     return f"""
-        SELECT DISTINCT 
-            DATE, 
-            AWAY_TEAM_ID, 
-            AWAY_GOALS,
-            HOME_TEAM_ID,
-            HOME_GOALS,
-            LENGTH_OF_GAME_MIN,
-            CASE WHEN 
-                HOME_GOALS = AWAY_GOALS AND HOME_GOALS = 0 AND AWAY_GOALS = 0
-                THEN 'UNKNOWN' ELSE OUTCOME END AS ACTUAL, 
-            PREDICTION
-        FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_STATS_PREDICTIONS 
+        SELECT *
+        FROM NHL_STATS.RAW.GAMES
         WHERE DATE BETWEEN '{start}' and '{end}'
         ORDER BY DATE DESC
     """
     
-def predictions_overview_query(start, end, away, home):
-    return f"""
-        SELECT *
-        FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_PREDICTIONS
-        WHERE DATE BETWEEN '{start}' and '{end}'
-            AND LOWER(AWAY_TEAM_ID) LIKE ('%{away.lower()}%')
-            AND LOWER(HOME_TEAM_ID) LIKE ('%{home.lower()}%')
-        ORDER BY DATE DESC
-    """
+# def predictions_overview_query(start, end, away, home):
+#     return f"""
+#         SELECT *
+#         FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_PREDICTIONS
+#         WHERE DATE BETWEEN '{start}' and '{end}'
+#             AND LOWER(AWAY_TEAM_ID) LIKE ('%{away.lower()}%')
+#             AND LOWER(HOME_TEAM_ID) LIKE ('%{home.lower()}%')
+#         ORDER BY DATE DESC
+#     """
 
-def predictions_full_query(start, end):
-    return f"""
-        SELECT * 
-        FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_PREDICTIONS
-        WHERE DATE BETWEEN '{start}' and '{end}'
-        ORDER BY DATE DESC
-    """
+# def predictions_full_query(start, end):
+#     return f"""
+#         SELECT * 
+#         FROM PC_DBT_DB.NHL_SEASON_STATS_AGG.NHL_SEASON_PREDICTIONS
+#         WHERE DATE BETWEEN '{start}' and '{end}'
+#         ORDER BY DATE DESC
+#     """
