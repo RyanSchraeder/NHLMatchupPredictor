858a4e15377e90f01f5a134272f0c134d4769ec4
Ryan Schraeder <rschraeder2@gmail.com>
2025-04-07 14:54:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,9 +120,10 @@ if st.button("Reset", type="primary"):
 st.subheader("Current Season Analysis")
 st.write("Current Top 10 Ranking Teams by Wins vs. Losses")
 fig = px.bar(top10, x="TEAM", y=["OVERALL_WINS", "OVERALL_LOSSES", "OVERTIME_LOSSES"])
-scores = px.bar(top10, x="TEAM", y=["GOALS_FOR", "GOALS_AGAINST"])
 st.plotly_chart(fig, key="top10teams")
+scores = px.bar(top10, x="TEAM", y=["GOALS_FOR", "GOALS_AGAINST"])
 st.plotly_chart(scores, key="top10teamsscoring")
+
 st.dataframe(top10, use_container_width=True)
              
 # Use the native Altair theme.
